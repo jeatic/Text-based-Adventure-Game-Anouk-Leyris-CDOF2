4ac076d1eb3c800c65152b948bad17df12ed374f
jeatic <122202333+jeatic@users.noreply.github.com>
2024-02-05 15:34:13 +0100

    Update project.py

--- project.py
+++ project.py
@@ -45,15 +45,14 @@ def create_question(question, choices):
     for i, choice in enumerate(choices, start=1):
         slow_print(f"{i}. {choice}")
 
-    while True:
-        try:
-            choice_num = int(input("Choose a number: "))
-            if 1 <= choice_num <= len(choices):
-                return choice_num
-            else:
-                slow_print("Please choose a valid number.")
-        except ValueError:
-            slow_print("Please enter a number.")
+   try:
+    choice_num = int(input("Choose a number: "))
+    if 1 <= choice_num <= len(choices):
+        return choice_num
+    else:
+        print("Please choose a valid number.")
+    except ValueError:
+    print("Please enter a number.")
 
 def encounter_creature():
     slow_print("You encounter a mysterious creature in the forest.")
